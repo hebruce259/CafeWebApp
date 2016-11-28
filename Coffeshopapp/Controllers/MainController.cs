a45ef2bf2bb51c10e8f2847aaae64b589dadf511
hebruce259 <saundrabruce@hotmail.com>
2016-11-28 09:40:10 -0500

    Adding new web app

---
+++ Coffeshopapp/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coffeshopapp.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProcessSignup(string Uname, string Email)
+        {
+            ViewBag.Message = "Thanks " + Uname + "(" + Email + ")";
+            // return View("Index"); return View("Index");
+            return Redirect("https://www.google.com");
+        }
+    }
+}
